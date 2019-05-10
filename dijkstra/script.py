fc3b3d9957bf69883aec4031f146773ce74f466a
Andi-Chin <39353554+Andi-Chin@users.noreply.github.com>
2019-05-09 23:31:40 -0400

    see the searching algorithm in real time

--- dijkstra/script.py
+++ dijkstra/script.py
@@ -34,33 +34,44 @@ n5 = Node(522, 305, 'n5')
 n6 = Node(662, 285, 'n6')
 n7 = Node(380, 407, 'n7')
 n8 = Node(511, 437, 'n8')
+n9 = Node(205, 474, 'n9')
+n10 = Node(424, 544, 'n10')
 
-
+Graph.addPath(Path(n9, n10))
+Graph.addPath(Path(n10, n8))
+Graph.addPath(Path(n9, n7))
 Graph.addPath(Path(n1, n5))
 Graph.addPath(Path(sourceNode, n2))
 Graph.addPath(Path(sourceNode, n3))
 Graph.addPath(Path(sourceNode, n4))
 Graph.addPath(Path(n4, n2))
 Graph.addPath(Path(n5, n4))
-Graph.addPath(Path(n5, endNode))
+# Graph.addPath(Path(n5, endNode))
 Graph.addPath(Path(n3, n1))
 Graph.addPath(Path(n5, n6))
 Graph.addPath(Path(n2, n6))
-Graph.addPath(Path(n6, endNode))
+# Graph.addPath(Path(n6, endNode))
 Graph.addPath(Path(n3, n7))
 Graph.addPath(Path(n7, n5))
 Graph.addPath(Path(n7, n8))
 Graph.addPath(Path(n8, endNode))
+Graph.addPath(Path(n3, n9))
 
 
-Graph.explore()
-Graph.tracePath()
+
+
 
 # TODO organize game gym events
 
 while True:
 
     screen.fill(Sett.BLACK)
+
+    if Graph.finishedSearching == True:
+        Graph.tracePath()
+    else: 
+        Graph.explore()
+
 
     Graph.draw(screen, myfont)
 
@@ -69,5 +80,8 @@ while True:
             sys.exit()
     pygame.display.flip()
 
+    print(pygame.mouse.get_pos())
+
+    time.sleep(2)
 
 
